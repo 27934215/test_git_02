20ff7d400fd9fce7af8035f4fe7cd4455bbcef2c
name <2799342155@qq.com>
2019-02-28 23:37:02 +0800

    test_git

---
+++ src/com/gitHub/TestGit01.java
@@ -0,0 +1,12 @@
+package com.gitHub;
+
+public class TestGit01 {
+
+    public static void main(String[] args) {
+        System.out.println("wwwwww");
+        int a=0;
+        while(a<10){
+            a++;
+        }
+    }
+}
---
+++ src/com/gitHub/TestGit02.java
@@ -0,0 +1,8 @@
+package com.gitHub;
+
+public class TestGit02 {
+
+    public void test(int a,int b){
+        System.out.println(a+b);
+    }
+}
